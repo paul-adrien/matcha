60b5070602fdf87f20e07f92cc00987a87187092
Gabin Guyot <gabin.guyot17@gmail.com>
2021-01-12 23:40:25 +0100

    add design on login-page, add form login

--- matchaFront/src/app/login/login-page.component.ts
+++ matchaFront/src/app/login/login-page.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   template: `
+  <div class="cow">
+    <img class="img" src="./assets/cow.svg">
+  </div>
+  <div class="cow-2">
+    <img class="img" src="./assets/cow.svg">
+  </div>
   <div class="logo">
     <img class="img" src="./assets/logo-matcha.svg">
     <img class="img" src="./assets/slogan.svg">
@@ -10,6 +16,15 @@ import { Component, OnInit } from '@angular/core';
     <div class="top-container">
       <div (click)="this.signInOrSignUp(false)" class="case" [class.select]="!this.loginMode">S'inscrire</div>
       <div (click)="this.signInOrSignUp(true)" class="case" [class.select]="this.loginMode">Se connecter</div>
+    </div>
+    <div class="form-container">
+      <input placeholder="Email">
+      <input placeholder="Password">
+      <input *ngIf="!this.loginMode" placeholder="Confirm Password">
+      <div *ngIf="this.loginMode">Se souvenir de moi</div>
+      <div class="primary-button">{{this.loginMode ? "Se connecter" : "Créer un compte"}}</div>
+      <div *ngIf="this.loginMode">Mot de passe oublié ?</div>
+
     </div>
   </div>
   `,
