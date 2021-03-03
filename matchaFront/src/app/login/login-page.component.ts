a4eec7148c36f1c1de76340b6497be8385cd9055
paul-adrien <paul.adrien.76@gmail.com>
2021-03-03 22:07:22 +0100

    modif form login page

--- matchaFront/src/app/login/login-page.component.ts
+++ matchaFront/src/app/login/login-page.component.ts
@@ -1,7 +1,30 @@
 import { Router } from "@angular/router";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { AuthService } from "../_service/auth_service";
 import { User } from "@matcha/shared";
+import { FormControl, FormGroup } from '@angular/forms';
+
+function ValidatorLength(control: FormControl) {
+  if (control.value?.length < 3) {
+    return { error: "3 caractères minimum" };
+  } else if (control.value?.length > 20) {
+    return { error: "20 caractères maximum" };
+  }
+}
+
+function ValidatorEmail(control: FormControl) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!re.test(String(control.value).toLowerCase())) {
+    return { error: "Mauvais format" };
+  }
+}
+
+function ValidatorPass(control: FormControl) {
+  const re = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  if (!re.test(String(control.value).toLowerCase())) {
+    return { error: "le mot de passe doit comporter minimum 8 caractères, un chiffre et un caractère spéciale" };
+  }
+}
 
 @Component({
   template: `
@@ -18,87 +41,130 @@ import { User } from "@matcha/shared";
         </div>
       </div>
       <form
+        [formGroup]="this.registerForm"
         class="form-container"
-        *ngIf="!isLoggedIn"
+        *ngIf="!isLoggedIn && !this.loginMode"
         name="form"
         (ngSubmit)="f.form.valid && onSubmit()"
         #f="ngForm"
         novalidate
       >
         <input
-          type="text"
-          class="form-control"
-          name="userName"
-          [(ngModel)]="form.userName"
-          minlength="3"
-          maxlength="20"
-          required
-          #userName="ngModel"
-          placeholder="Nom d'utilisateur"
+            type="text"
+            formControlName="userName"
+            id="userName"
+            required
+            placeholder="Nom d'utilisateur"
         />
+        <div class="error" *ngIf="this.registerForm.get('userName').errors?.error && this.isSuccessful === false">
+          {{ this.registerForm.get("userName").errors.error }}
+        </div>
         <input
           *ngIf="!this.loginMode"
           type="text"
-          class="form-control"
-          name="lastName"
-          [(ngModel)]="form.lastName"
+          formControlName="lastName"
           required
-          minlength="3"
-          maxlength="20"
-          #lastName="ngModel"
           placeholder="Nom"
         />
+        <div class="error" *ngIf="this.registerForm.get('lastName').errors?.error && !this.loginMode && this.isSuccessful === false">
+          {{ this.registerForm.get("lastName").errors.error }}
+        </div>
         <input
           *ngIf="!this.loginMode"
           type="text"
-          class="form-control"
-          name="FirstName"
-          [(ngModel)]="form.firstName"
+          formControlName="firstName"
           required
-          minlength="3"
-          maxlength="20"
-          #firstName="ngModel"
           placeholder="Prénom"
         />
+        <div class="error" *ngIf="this.registerForm.get('firstName').errors?.error && !this.loginMode && this.isSuccessful === false">
+          {{ this.registerForm.get("firstName").errors.error }}
+        </div>
         <input
           *ngIf="!this.loginMode"
-          type="email"
-          class="form-control"
-          name="email"
-          [(ngModel)]="form.email"
+          formControlName="email"
           required
-          email
-          #email="ngModel"
           placeholder="Email"
         />
+        <div class="error" *ngIf="this.registerForm.get('email').errors?.error && this.isSuccessful === false && !this.loginMode">
+          {{ this.registerForm.get("email").errors.error }}
+        </div>
         <input
+          *ngIf="!this.loginMode"
           type="password"
-          class="form-control"
-          name="password"
-          [(ngModel)]="form.password"
+          formControlName="password"
           required
-          minlength="6"
-          #password="ngModel"
-          placeholder="Password"
+          placeholder="Mot de passe"
         />
-        <div *ngIf="this.loginMode">Se souvenir de moi</div>
-        <button class="primary-button">
+        <div class="error" *ngIf="this.registerForm.get('password').errors?.error && this.isSuccessful === false && !this.loginMode">
+          {{ this.registerForm.get("password").errors.error }}
+        </div>
+        <input
+          *ngIf="this.loginMode"
+          type="password"
+          formControlName="passwordLogin"
+          required
+          placeholder="Mot de passe"
+        />
+        <button class="primary-button" (click)="test()">
           {{ this.loginMode ? "Se connecter" : "Créer un compte" }}
         </button>
-        <a *ngIf="this.loginMode" routerLink="/forgotPass" routerLinkActive="active"
-          >Mot de passe oublié ?</a
-        >
-        <p>{{ errorMessage }}</p>
       </form>
+      <form
+        [formGroup]="this.loginForm"
+        class="form-container"
+        *ngIf="!isLoggedIn && this.loginMode"
+        name="form"
+        (ngSubmit)="f.form.valid && onSubmit()"
+        #f="ngForm"
+        novalidate
+      >
+        <input
+            type="text"
+            formControlName="userName"
+            id="userName"
+            required
+            placeholder="Nom d'utilisateur"
+        />
+        <div class="error" *ngIf="this.loginForm.get('userName').errors?.error && this.isSuccessful === false">
+          {{ this.loginForm.get("userName").errors.error }}
+        </div>
+        <input
+          type="password"
+          formControlName="password"
+          required
+          placeholder="Mot de passe"
+        />
+        <button class="primary-button" (click)="test()">
+          {{ this.loginMode ? "Se connecter" : "Créer un compte" }}
+        </button>
+      </form>
+      <a *ngIf="this.loginMode" routerLink="/forgotPass" routerLinkActive="active"
+        >Mot de passe oublié ?
+      </a>
+      <p>{{ errorMessage }}</p>
     </div>
   `,
   styleUrls: ["./login-page.component.scss"],
   selector: "login-page",
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginPageComponent implements OnInit {
+
+  public registerForm = new FormGroup({
+    userName: new FormControl("", ValidatorLength),
+    firstName: new FormControl("", ValidatorLength),
+    lastName: new FormControl("", ValidatorLength),
+    password: new FormControl("", ValidatorPass),
+    email: new FormControl("", ValidatorEmail)
+  });
+
+  public loginForm = new FormGroup({
+    userName: new FormControl("", ValidatorLength),
+    password: new FormControl("")
+  });
+
   public loginMode = false;
-  form: Partial<User> = {};
-  isSuccessful = false;
+  isSuccessful = true;
   isSignUpFailed = false;
   isLoggedIn = false;
   isLoginFailed = false;
@@ -112,9 +178,14 @@ export class LoginPageComponent implements OnInit {
     this.loginMode = loginMode;
   }
 
+  test() {
+    this.isSuccessful = false;
+  }
+
   onSubmit() {
     if (this.loginMode === false) {
-      this.authService.register(this.form).subscribe(
+      const form: Partial<User> = this.registerForm.getRawValue();
+      this.authService.register(form).subscribe(
         data => {
           console.log(data);
           if (data.status === true) {
@@ -132,7 +203,8 @@ export class LoginPageComponent implements OnInit {
         }
       );
     } else {
-      this.authService.login(this.form).subscribe(
+      const form: Partial<User> = this.loginForm.getRawValue();
+      this.authService.login(form).subscribe(
         data => {
           console.log(data);
           if (data.status === true) {
